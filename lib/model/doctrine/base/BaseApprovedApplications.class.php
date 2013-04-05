d6f8c536c884b1ce2bf225038e3b2cc11bf6d03c
gabsotb <gabssmall@gmail.com>
2013-04-05 11:31:37 +0300

    merge aftermath

--- lib/model/doctrine/base/BaseApprovedApplications.class.php
+++ lib/model/doctrine/base/BaseApprovedApplications.class.php
@@ -42,10 +42,10 @@ abstract class BaseApprovedApplications extends sfDoctrineRecord
              'notnull' => true,
              'length' => 255,
              ));
-        $this->hasColumn('comment', 'string', 255, array(
+        $this->hasColumn('comment', 'string', 1000, array(
              'type' => 'string',
              'notnull' => false,
-             'length' => 255,
+             'length' => 1000,
              ));
         $this->hasColumn('token', 'string', 255, array(
              'type' => 'string',
